8e05e11a3304dd1ad195bb19b5175123a7852766
Diego Pereira <diegoteixeirapereira@hotmail.com>
2025-01-29 10:42:57 -0300

    criação da tela e comida do jogo

--- test/main.py
+++ test/main.py
@@ -1,21 +1,66 @@
 import pygame
-# import Randon
+import random
 
 pygame.init() 
 
 pygame.display.set_caption("Jogo da Cobra em Pythom")
 
-largura, altura = 1280, 720
+largura, altura = 1280, 700
 
 tela = pygame.display.set_mode((largura, altura))
 
 preta = (0, 0, 0)
 
+branca = (255, 255, 255)
+
+vermelho = (255, 0, 0)
+
+verde = (0, 255, 0)
+
+tamanho_quadrado = 20
+
+velocidade_jogo = 15
+
+def desenhar_cobra(tamanho, pixels):
+
+            for pixel in pixels:
+
+                pygame.draw.rect(tela, branca, [pixel[0], pixel[1], tamanho, tamanho])
+
+def gerar_comida():
+
+    comida_x = round(random.randrange(0, largura - tamanho_quadrado) / 20.0) * 20.0
+
+    comida_y = round(random.randrange(0, altura - tamanho_quadrado) / 20.0) * 20.0
+
+    return comida_x, comida_y
+
+def desenhar_comida(tamanho, comida_x, comida_y):
+
+    pygame.draw.rect(tela, verde, [comida_x, comida_y, tamanho, tamanho])
+
+
 def rodar_jogo(): 
     
     fim_jogo = False
 
+    x = largura / 2
+
+    y = altura / 2
+
+    velocidade_x = 0
+
+    velocidade_y = 0
+
+    tamanho_cobra = 1
+
+    pixels = []
+
+    comida_x, comida_y = gerar_comida()
+
     while not fim_jogo:
+           
+        #    pass
 
         tela.fill(preta)
 
@@ -25,5 +70,33 @@ def rodar_jogo():
 
                 fim_jogo = True
 
+        
+        desenhar_comida(tamanho_quadrado, comida_x, comida_y)
+
+        pygame.display.update()
+
+        
+        # pixels.append([x, y])
+
+        # if len(pixels) > tamanho_cobra:
+
+        #     del pixels[0]
+
+
+        # for pixel in pixels[:-1]:
+             
+        #      if pixel == [x, y]:
+                  
+        #           fim_jogo = True
+
+             
+
+
+        # desenhar_cobra(tamanho_quadrado, pixels)
+
+
+
+
 
 rodar_jogo()
+
